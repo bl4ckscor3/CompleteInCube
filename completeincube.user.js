9f523fe8f0eef53789875afca1ea33a20d92102e
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-02-26 17:21:22 +0100

    make more actions available in cube

--- completeincube.user.js
+++ completeincube.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Complete In Cube
 // @namespace    bl4ckscor3
-// @version      1.0.1
+// @version      1.1
 // @description  Eyewire script to make the standard "c+c" shortcut for completing a cube available while inspecting a cube
 // @author       bl4ckscor3
 // @match        https://eyewire.org/
@@ -29,27 +29,35 @@
     }, 100);
 
     function main() {
-        let pressedC = false;
+        addFunctionality(() => $.post(`1.0/task/${tomni.task.id}`, {action: "complete"}, "json"), 'C', 'c'); //complete the cube
+        addFunctionality(() => $.post(`1.0/task/${tomni.task.id}`, {action: "uncomplete"}, "json"), 'U', 'u'); //uncomplete the cube
+        addFunctionality(() => $.post(`1.0/task/${tomni.task.id}`, {action: "freeze"}, "json"), 'F', 'f'); //un-/freeze the cube
+        addFunctionality(() => $("#cubeInspectorFloatingControls > div.controls > div.control.custom-highlight > button").click(), 'V', 'v'); //highlight the cube
+        addFunctionality(() => $("#cubeInspectorFloatingControls > div.controls > div.control.custom-unhighlight > button").click(), 'B', 'b'); //unhighlight the cube
 
-        $(document).keydown(e => {
-            if(e.key == 'C' || e.key == 'c') {
-                //ignore if the user is currently not inspecting a cube
-                //ignore if the user is currently typing into chat
-                if(tomni.task && tomni.task.inspect && !document.getElementById("chatContainer").children[0].classList.contains("active")) {
-                    if(pressedC) { //second time pressing c
-                        $.post(`1.0/task/${tomni.task.id}`, {action: "complete"}, "json"); //complete the cube
-                        tomni.leave(); //return to overview
-                        pressedC = false;
+        function addFunctionality(action, ...keys) {
+            let hasPressedKey = false;
+
+            $(document).keydown(e => {
+                if(keys.some(k => k == e.key)) {
+                    //ignore if the user is currently not inspecting a cube
+                    //ignore if the user is currently typing into chat
+                    if(tomni.task && tomni.task.inspect && !document.getElementById("chatContainer").children[0].classList.contains("active")) {
+                        if(hasPressedKey) { //second time pressing the key
+                            action();
+                            tomni.leave(); //return to overview
+                            hasPressedKey = false;
+                        }
+                        else { //first time pressing the key
+                            hasPressedKey = true;
+                        }
+                        
+                        return;
                     }
-                    else { //first time pressing c
-                        pressedC = true;
-                    }
-                    
-                    return;
                 }
-            }
-            
-            pressedC = false;
-        });
+                
+                hasPressedKey = false;
+            });
+        }
     }
 })();
